6c2319bd4ee84957227fb0a56e38e71db7a65e1c
Stone Tao <stonezt2019@gmail.com>
2021-01-18 11:31:42 -0800

    sprint tuna

---
+++ src/sprinttuna/utils/HashMapNodeVal.java
@@ -0,0 +1,19 @@
+package sprinttuna.utils;
+// using this as node values, we can compare keys and store values still as linkedlist contain method uses .equals
+public class HashMapNodeVal<K,V> {
+    public K key;
+    public V val;
+    HashMapNodeVal(K key, V val) {
+        this.key = key;
+        this.val = val;
+    }
+    @Override
+    public boolean equals(Object o) {
+        return this.key.equals(((HashMapNodeVal<K, V>) o).key);
+    }
+    
+    @Override
+    public String toString() {
+        return "K: " + key.toString() + " - V: " + val.toString();
+    }
+}
