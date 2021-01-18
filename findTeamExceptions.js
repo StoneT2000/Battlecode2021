2e46704ca71a4ffc102705ea35ad204dc6da6c2c
Stone Tao <stonezt2019@gmail.com>
2021-01-18 12:30:19 -0800

    Update findTeamExceptions.js

--- findTeamExceptions.js
+++ findTeamExceptions.js
@@ -1,1 +1,19 @@
 const fs = require('fs');
+
+const find = (filename, team = "A") => {
+  const content = `${fs.readFileSync(filename)}`.split("\n");
+  let i = 0;
+  for (const line of content) {
+    if (line[1] == team) {
+      if (line.search("Exception") != -1) {
+        console.log(line);
+
+      if (line + 10 < content.length) {
+        console.log(content.slice(i, i+10))
+      }
+      }
+    }
+    i++;
+  }
+}
+find("output");
