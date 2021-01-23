a11f3e6ba60b9fd87bbacb8e02cbd981236e4366
Stone Tao <stonezt2019@gmail.com>
2021-01-23 13:31:23 -0800

    confused sushi

---
+++ src/confusedsushi/ECDetails.java
@@ -0,0 +1,15 @@
+package skepticalsushi;
+
+import battlecode.common.*;
+
+public class ECDetails {
+    public MapLocation location;
+    /** may be -1, signifying we dont have info on this */
+    public int lastKnownConviction = -1;
+    public int teamind = -1;
+    ECDetails(MapLocation loc, int lastKnownConviction, int teamind) {
+        this.location = loc;
+        this.lastKnownConviction = lastKnownConviction;
+        this.teamind = teamind;
+    }
+}
